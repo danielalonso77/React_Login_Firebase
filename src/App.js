ad4073d76f73d389dfaaf49a08571ee87b1f99a8
danielalonso77 <54615444+danielalonso77@users.noreply.github.com>
2022-05-27 18:19:49 -0500

    open setopen
    cambio de estado en

--- src/App.js
+++ src/App.js
@@ -5,36 +5,33 @@ import Login from "./components/Login";
 import { ProtectedRoutes } from "./components/ProtectedRoutes";
 import Register from "./components/Register";
 import { AuthProvider } from "./context/authContext";
+import Grafica1 from "./graficas/Grafica1";
+import Grafica2 from "./graficas/Grafica2";
+import Grafica3 from "./graficas/Grafica3";
 
 const App = () => {
-    return (
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+                <div className="bg-pink-200">
 
-        <
-        AuthProvider >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { <
-            ProtectedRoutes >
-            <
-            Home / >
-            <
-            /ProtectedRoutes>
-        }
-        /> <
-        Route path = "/login"
-        element = { < Login / > }
-        /> <
-        Route path = "/register"
-        element = { < Register / > }
-        /> <
-        /Routes> <
-        /AuthProvider>
-
-    );
+                </div>
+              <Home/>
+            </ProtectedRoutes>
+          }
+        />{" "}
+        <Route path="/login" element={<Login />} />{" "}
+        <Route path="/register" element={<Register />} />{" "}
+        <Route path="/grafics1" element={<Grafica1/>} />{" "}
+        <Route path="/grafics2" element={<Grafica2/>} />{" "}
+        <Route path="/grafics3" element={<Grafica3/>} />{" "}
+      </Routes>{" "}
+    </AuthProvider>
+  );
 };
-
-
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 /* import { useNavigate } from "react-router-dom"; */
 
+//importar el compontente de UI transicion
+import { Transition } from "@headlessui/react";
 import image from "../logo_GO.png";
 import { useAuth } from "../context/authContext";
 
@@ -15,6 +17,8 @@ export default function Home() {
 
   /* const navigate = useNavigate(); */
 
+  const [open, setOpen] = useState(false);
+
   const { user, logout, loading } = useAuth();
   console.log(user);
 
@@ -26,62 +30,115 @@ export default function Home() {
     }
   };
 
+  const handleOpen = () => {
+    if (open) {
+      document.getElementById("tabOpen").hidden = true;
+      setOpen(false);
+    } else {
+      setOpen(true);
+      document.getElementById("tabOpen").hidden = false;
+    }
+  };
+
+  function Ventanita() {
+    return (
+      <div className="bg-pink-200">
+        <div className="flex justify-center">
+          <div className="flex flex-col items-center">
+            <div className="flex justify-center">
+              <img src={image} className="h-16 w-16" alt="logo" />
+            </div>
+            <h1 className="text-center text-2xl font-bold">
+              Bienvenido {user.name}
+            </h1>
+            <div className="flex justify-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={handleLogout}
+              >
+                Cerrar Sesión
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   if (loading) {
     return (
       <div>
-        <h1> Cargando.. </h1>{" "}
+        <h1> Cargando.. </h1>
       </div>
     );
   }
 
   return (
     <>
-      <div className="w-full h-full ">
-        <div className="">
-          <div className="items-center d-block sticky  ">
-            
-            <ul className="flex">
-              <li className="mr-6"></li>
-              <li className="mr-6">
-                <img
-                  src={image}
-                  alt="google"
-                  className="w-10 h-10 felx items-left"
-                />
-              </li>{" "}
-              <li className="mr-6">
-                <h1> Grafica1 </h1>{" "}
-              </li>{" "}
-              <li className="mr-6">
-                <h1> Grafica2 </h1>{" "}
-              </li>{" "}
-              <li className="mr-6">
-                <h1> Grafica3 </h1>{" "}
-              </li>{" "}
-              <li className="mr-6">
-                <h1> Grafica4 </h1>{" "}
-              </li>{" "}
-              <li className="mr-6">
+      <nav className="flex px-6 h-16 bg-blue-400 text-gray-700 border-b border-gray-200 z-10">
+        <ul className="flex mr-48">
+          <li className="mr-6">
+            <img
+              src={image}
+              alt="google"
+              className="bg-blue-400 w-10 h-10 felx"
+            />
+          </li>
+          <li className="mr-6">
+            <h1>
+              {" "}
+              <a href="/grafics1">Grafica1</a>{" "}
+            </h1>
+          </li>
+          <li className="mr-6">
+            <h1>
+              {" "}
+              <a href="/grafics2">Grafica2</a>{" "}
+            </h1>
+          </li>
+          <li className="mr-6">
+            <h1>
+              {" "}
+              <a href="/grafics3">Grafica3</a>{" "}
+            </h1>
+          </li>
+          <li className="mr-6">
+            <h1> Grafica4 </h1>
+          </li>
+        </ul>
 
-                <img
-                  alt="myself"
-                  src={user.photoURL}
-                  className="rounded-full w-10 h-10"
-                />
-                <p> Hola {user.displayName.split(" ", [1]) || user.email} </p>
-                <button className="bg-blue-400 " onClick={handleLogout}>
-                  cerar sesion
-                </button>
-              </li>{" "}
-            </ul>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
+        <ul className="flex">
+         
+          <li className="ml-6">
+            <p className="text-xs ml-6">
+              {" "}
+              Hola {user.displayName.split(" ", [1]) || user.email}{" "}
+            </p>
+          </li>
+          <ul id="tabOpen" className="bg-white ">
+            <li id="tabOpen" className="bg.white">
+              <button
+                className="bg-blue-600 hover:bg-blue-800 hover:text-slate-100 rounded-lg text-s mb-1 px-2"
+                onClick={handleLogout}
+              >
+                cerar sesion
+              </button>
+            </li>
+          </ul>
+          <li className="ml-6 px-10">
+            <img
+              alt="myself"
+              src={user.photoURL}
+              onClick={() => handleOpen()}
+              className="rounded-full w-10 h-10"
+            />
+          </li>
+        </ul>
+      </nav>
       {/* <a className="bg-blue-200n text-blue"  href="https://www.systemsorion.com/Desperdicios/vistas/indexPrincipal.php">
                                                                                                                                                                                                                                                   {" "}
                                                                                                                                                                                                                                                   Sistemas de desperdicio
                                                                                                                                                                                                                                                 </a> */}{" "}
-      <div className="bg-white w-auto h-96 "> </div>{" "}
     </>
   );
 }
